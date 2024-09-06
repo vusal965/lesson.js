d8f93451c1eb1e46490455a2c48109692541f81f
vusal965 <vusalbaxsiyev756@gmail.com>
2024-09-06 14:15:39 +0400

    Home work

---
+++ app.js
@@ -0,0 +1,232 @@
+// let id = +prompt("mehsul id daxil edin:")
+// switch (id){
+//     case 1:
+//         console.log("Movcud 10 eded");
+//         break;
+// case 2:
+//     console.log("Movcud 256 eded.");
+//     break;
+// case 3:
+//     console.log("53 eded movcuddur");
+//     break;
+// case 4:
+//     console.log("Movcud 3 eded");
+//     break;
+
+//     default:
+//         console.log("Movcud deyil");
+//         break;
+// }
+
+// let result = prompt("cinsinizi yazin M ve ya F")
+// let male = ("Male")
+// let female = ("Female")
+// result.startsWith("M") ? console.log("Male") : console.log("M");
+// result.startsWith("F") ? console.log("Female") : console.log("F");
+
+// function greeting() {
+//     console.log('"Vusal is a JS developer"');
+// }
+// greeting();
+// function userName(name) {
+//     console.log(name + " is active now.");
+    
+// }
+
+// userName("Vusal")
+// userName("Rahim")
+// userName("Ali")
+// let a = 8;
+// let b = 5;
+// let c = 3;
+// function result( a = 8, +b, c){
+//     console.log(+a > b) && (+c < +a);
+// }
+// result();
+
+
+// let num = +prompt("iki reqemli eded daxil edin:")
+
+// while((num +7) < 100) {
+//     num +=7;
+//     console.log(num);
+// }
+
+// let n = +prompt("eded daxil edin:")
+
+// while(n > 0){
+//     n--;
+//     console.log("This is cycle");
+
+// }
+
+
+// for(let num = 0; num < 10; num++){
+//    let num2 = num * 1; 
+// console.log(num2);
+// }
+
+
+// let text = prompt("metn daxil edin:")
+// let result = " ";
+// for(let i = text.length -1; i >= 0; i--) {
+//     result += text[i]
+// // console.log(text[i]);
+// }
+// console.log(result);
+
+
+
+
+
+
+
+// let arr = ["Vusal", "Kenan", "Rasim"]
+// arr[1] = "Qalib"
+// console.log(arr);
+
+
+// let arr = ["New Hope", "Empire strikes back", "Return of the Jdi"]
+
+// arr[0] = "Episode 4: New Hope"
+// arr[1] = "Episode 5: Empire strikes back"
+// arr[2] = "Episode 6: Return of the Jdi"
+
+
+
+// for (let i = 0; i < arr.length; i++) {
+   
+//     console.log(arr[i]);
+    
+// }
+
+
+
+
+
+
+// let arr = ["Vusal", "Qalib", "Rasim", "Heyder", "Ferid"]
+
+// arr.shift();
+// arr.push("Hemid")
+// console.log(arr);
+
+
+// let data = ['41','42','43','44','45']
+
+//  let newData = data.join('; ')
+//  console.log(newData);
+ 
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let [num1, num2, num3, ...numbers] =arr;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(numbers);
+
+
+
+// let arr = [2, 3, -2, 0, -1]
+
+// function callBack(a){
+    // console.log(a);
+    // if(0 > a){
+        // console.log(a);
+        
+    // }
+    
+// }
+// arr.forEach(callBack)
+
+// let arr = [2, 3, -2, 0, -1]
+
+// arr.forEach(a => {
+    
+//     if(0 < a){
+//         console.log(a);
+        
+//     }
+    
+// })
+
+
+// let arr = [10, 20, 30, 40, 50,-60, -70, -80]
+//  arr.map((a) => {
+//     // return a * 2
+//     console.log(a.replace() +);
+    
+// } 
+// )
+
+// let arr = ["Green", "Red", "Blue"]
+
+// let a = arr.find(b => {
+//     return b.length == 3
+// }
+
+// )
+// console.log(a);
+
+
+// console.log(b);
+
+
+// let user = {
+//     setAge:  45,
+//     getYearsBeforeRetirement:68,
+// }
+// resultAge: () =>{
+//     console.log(this.setAge - this.getYearsBeforeRetirement);
+     
+
+// }
+
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     celebrateBirthday(){
+//         this.age += 1;
+//     }
+
+   
+// }
+
+// let result = new Person("vusal", 24)
+
+//  result.celebrateBirthday();
+//  console.log(result);
+ 
+
+
+
+// class Person{
+//     constructor(surname, name, age){
+//         this.surname = surname;
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// class Admin extends Person{
+//     constructor(surname, name, age, role, salary){
+//         super(surname, name, age,)
+//         this.role = role;
+//         this.salary = salary;
+//     }
+
+// }
+// class Player extends Person{
+//     constructor(surname, name, age,id){
+//         super(surname, name, age, )
+//         this.id = id;
+//     }
+// }
+// let person1 = new Person("aliyev", "ali", 24)
+
+
+
+// let h1 = document.querySelector("h1");
+// h1.remove();
+// body.append("RTX3000")
